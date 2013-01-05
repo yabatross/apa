d74b34f5834356f1cdf637e58065a70d7981b120
Alejandro Fernández (BSC) <alejandro.fernandez@bsc.es>
2013-01-05 16:46:27 +0100

    Red neuronal casi terminada, falta comentar los resultados con una semilla aleatoria fija para la inicializacion, de lo contrario siempre salen diferentes

--- nnet.R
+++ nnet.R
@@ -5,70 +5,54 @@
 library(MASS)
 library(nnet)
 
-# 1 capa oculta, 3 unitats ocultes, sense regularitzar, funció d'error "cross-entropy"
-# 
-neural.nnAll <- nnet(ds$X..50K
+# Part 1: Fem servir totes les variables d'entrada
+
+set.seed(3)
+
+neural.maxHiddenUnits <- 10
+
+neural.netHitsAll <- seq(from=1, to=neural.maxHiddenUnits)
+
+# Per accedir de forma segura a la taula quan només té una columna
+neural.safeaccess <- function (x) { if (dim(x)[2] == 2) return (x[2, "1"]); return (0) }
+
+# Esbrinem el millor nombre de unitats ocultes a la xarxa MLP
+# 1 sola capa, funció d'error cross-entropy
+for (i in 1:neural.maxHiddenUnits)
+{
+	neural.nnAll <- nnet(ds$X..50K
                 ~ ds$X39 + ds$State.gov + ds$X77516 + ds$Bachelors + ds$X13 + ds$Never.married + ds$Adm.clerical
                 + ds$Not.in.family + ds$White + ds$Male + ds$X2174 + ds$X0 + ds$X40 + ds$United.States
-                , data=ds,size=3,decay=0,maxit=2000,trace=T)
+                , data=ds,size=i,decay=0,maxit=2000,trace=T)
 
-#yhat <- as.numeric(predict(nn1,type='class'))
+	neural.resAll <- table(actual=ds$X..50K,predicted=predict(neural.nnAll,type='class'))
+	
+	# Calculem la proporcio d'encerts
+	neural.netHitsAll[i] <- (neural.resAll[1] + neural.safeaccess(neural.resAll)) / dim(ds)[1]
+}
 
+# Resultat obtingut usant totes les variables d'entrada
+neural.netHitsAll
 
-#par(mfrow=c(1,2))
-#plot(x,pch=19,col=c('red','blue')[y+1],main='actual labels',asp=1)
-#plot(x,col=c('red','blue')[(yhat>0.5)+1],pch=19,main='predicted labels',asp=1)
-#table(actual=y,predicted=predict(nn1,type='class'))
+# Part 2: Fem servir nomes les variables que millor expliquen el resultat
+# Veure apartat de filtratge a l'arxiu glm.R
+neural.netHitsRelevant <- seq(from=1, to=neural.maxHiddenUnits)
 
-# Executem de nou amb seed=4 ...
-#set.seed(4)
-#nn1 <- nnet(y~x.1+x.2,data=mydata,entropy=T,size=3,decay=0,maxit=2000,trace=T)
-#yhat <- as.numeric(predict(nn1,type='class'))
-#par(mfrow=c(1,2))
-#plot(x,pch=19,col=c('red','blue')[y+1],main='actual labels',asp=1)
-#plot(x,col=c('red','blue')[(yhat>0.5)+1],pch=19,main='predicted labels',asp=1)
-#table(actual=y,predicted=predict(nn1,type='class'))
+# Esbrinem el millor nombre de unitats ocultes a la xarxa MLP
+# 1 sola capa, funció d'error cross-entropy
+for (i in 1:neural.maxHiddenUnits)
+{
+	neural.nnRelevant <- nnet(ds$X..50K
+                ~ ds$X39 + ds$State.gov + ds$X77516 + ds$Bachelors + ds$X13 + ds$Never.married + ds$Adm.clerical
+                + ds$Not.in.family + ds$White + ds$Male + ds$X2174 + ds$X0 + ds$X40
+                , data=ds,size=i,decay=0,maxit=2000,trace=T)
 
-# ja veiem que l'optimitzador no sempre troba una bona solució
+	neural.resRelevant <- table(actual=ds$X..50K,predicted=predict(neural.nnRelevant,type='class'))
+	
+	# Calculem la proporcio d'encerts
+	neural.netHitsRelevant[i] <- (neural.resRelevant[1] + neural.safeaccess(neural.resRelevant)) / dim(ds)[1]
+}
 
-# Quantes unitats ocultes necessitem?
+# Resultat obtingut usant nomes les variables que millor expliquen el resultat
+neural.netHitsRelevant
 
-#par(mfrow=c(2,2))
-#for (i in 1:4)
-#{
-#  set.seed(3)
-#  nn1 <- nnet(y~x.1+x.2,data=mydata,entropy=T,size=i,decay=0,maxit=2000,trace=T)
-#  yhat <- as.numeric(predict(nn1,type='class'))
-#  plot(x,pch=20,col=c('black','green')[yhat+1])
-#  title(main=paste('nnet with',i,'hidden units'))
-#}
-
-# esbrinem quina és la funció que realment s'està calculant
-
-#set.seed(3)
-#nn1 <- nnet(y~x.1+x.2,data=mydata,entropy=T,size=3,decay=0,maxit=2000,trace=T)
-#x1grid <- seq(-3,3,l=200)
-#x2grid <- seq(-3,3,l=220)
-#xg <- expand.grid(x1grid,x2grid)
-#xg <- as.matrix(cbind(1,xg))
-#h1 <- xg%*%matrix(coef(nn1)[1:3],ncol=1)
-#h2 <- xg%*%matrix(coef(nn1)[4:6],ncol=1)
-#h3 <- xg%*%matrix(coef(nn1)[7:9],ncol=1)
-
-#par(mfrow=c(2,2))
-#contour(x1grid,x2grid,matrix(h1,200,220),levels=0)
-#contour(x1grid,x2grid,matrix(h2,200,220),levels=0,add=T)
-#contour(x1grid,x2grid,matrix(h3,200,220),levels=0,add=T)
-#title(main='boundaries from linear functions\n in hidden units')
-
-#sigmoid <- function(x){exp(x)/(1+exp(x))}
-#z <- coef(nn1)[10]+coef(nn1)[11]*sigmoid(h1)+coef(nn1)[12]*sigmoid(h2)+
-#  coef(nn1)[13]*sigmoid(h3)
-#contour(x1grid,x2grid,matrix(z,200,220))
-#title('sum of sigmoids \n of linear functions')
-#contour(x1grid,x2grid,matrix(sigmoid(z),200,220),levels=0.5)
-#title('sigmoid of sum of sigmoids \n of linear functions')
-#contour(x1grid,x2grid,matrix(sigmoid(z),200,220),levels=0.5)
-#points(x,pch=20,col=c('black','green')[y+1])
-#title('data values')
-#
