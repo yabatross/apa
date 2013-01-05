c58cdeef1bb9ee512d54eadc86e47b0934ba21f0
yabatross <jeannekamikaze@gmail.com>
2013-01-05 13:22:27 +0100

    Primera prediccion

--- glm.R
+++ glm.R
@@ -9,17 +9,17 @@ ds$X..50K <- factor (ds$X..50K)
 
 # Construim el model amb totes les variables.
 
-mylogit <- glm (df$X..50K
-                ~ df$X39 + df$State.gov + df$Gender + df$Age + df$Studying + df$Contract + df$Firmtype
-                + df$Accgrade + df$Grade + df$Startwork + df$Image + df$Exp.gene + df$Exp.spec + df$Qual.gen
-                + df$Qual.spec + df$Value + df$Satisfaction, data=df, family="binomial")
+mylogit <- glm (ds$X..50K
+                ~ ds$X39 + ds$State.gov + ds$X77516 + ds$Bachelors + ds$X13 + ds$Never.married + ds$Adm.clerical
+                + ds$Not.in.family + ds$White + ds$Male + df$X2174 + ds$X0 + df$X40 + ds$United.States
+                , data=ds, family="binomial")
 
 # A continuacio fem una prediccio i mesurem l'error.
 # En les nostres proves l'error es de 0.2284495, es a dir, que el model encerta un 77% dels cops.
 
 pr <- predict(mylogit, df, type="response")
 minus1 <- function(x) { return (x-1); }
-sa <- sapply(as.numeric(df$Salary), minus1)
+sa <- sapply(as.numeric(df$X..50K), minus1)
 err <- mean(abs(pr-sa))
 
 # Aqui fem el mateix, pero establim que si la prediccio es <0.5, la interpreten com un 0, i 1 d'altre manera.
@@ -38,51 +38,51 @@ err2 <- mean(abs(pr2-sa))
 # variables i fem una prediccio.
 #
 
-require(FSelector)
-require(mlbench)
-require(MASS)
+#require(FSelector)
+#require(mlbench)
+#require(MASS)
 
 #Preparing data frame
-x <- cbind(  df$Num, df$Career, df$Gender, df$Age, df$Studying, df$Contract,
-             df$Firmtype, df$Accgrade, df$Grade, df$Startwork, df$Image, df$Exp.gene, 
-             df$Exp.spec, df$Qual.gen, df$Qual.spec, df$Value, df$Satisfaction)
+#x <- cbind(  df$Num, df$Career, df$Gender, df$Age, df$Studying, df$Contract,
+#             df$Firmtype, df$Accgrade, df$Grade, df$Startwork, df$Image, df$Exp.gene, 
+#             df$Exp.spec, df$Qual.gen, df$Qual.spec, df$Value, df$Satisfaction)
 
-y <- cbind(df$Salary)
+#y <- cbind(df$Salary)
 
 # The predictors are centered and scaled:
-x <- as.data.frame(scale(x))
+#x <- as.data.frame(scale(x))
 
 # Form final data frame
-fullset.formula <- as.simple.formula(colnames(x), "Target")
-dataforFSS <- cbind (x,y)
-colnames(dataforFSS)[18] <- "Target"
+#fullset.formula <- as.simple.formula(colnames(x), "Target")
+#dataforFSS <- cbind (x,y)
+#colnames(dataforFSS)[18] <- "Target"
 
 # Linear Regression
-LinearRegression <- glm (Target~., family = gaussian, data = dataforFSS)
-subset.LinearRegression.formula <- step(LinearRegression)$formula
+#LinearRegression <- glm (Target~., family = gaussian, data = dataforFSS)
+#subset.LinearRegression.formula <- step(LinearRegression)$formula
 
 # Showing filtered formula to show most significant dependencies
 # Target ~ V1 + V3 + V6 + V7 + V8 + V14 + V16
 # Salary ~ Gender + Age + Contract + Image + Exp.spec + Qual.gen + Value + Satisfaction
-print(subset.LinearRegression.formula)
+#print(subset.LinearRegression.formula)
 
 
 # Construim un model amb les variables rellevants.
 
-mylogit <- glm (df$Salary
-                ~ df$Gender + df$Age + df$Contract + df$Image + df$Exp.spec + df$Qual.gen
-                + df$Value + df$Satisfaction, data=df, family="binomial")
+#mylogit <- glm (df$Salary
+#                ~ df$Gender + df$Age + df$Contract + df$Image + df$Exp.spec + df$Qual.gen
+#                + df$Value + df$Satisfaction, data=df, family="binomial")
 
 # A continuacio fem una prediccio i mesurem l'error.
 # En les nostres proves l'error es de 0.254356, lleugerament diferent de err1.
 
-pr3 <- predict(mylogit, df, type="response")
-sa3 <- sapply(as.numeric(df$Salary), minus1)
-err3 <- mean(abs(pr3-sa3))
+#pr3 <- predict(mylogit, df, type="response")
+#sa3 <- sapply(as.numeric(df$Salary), minus1)
+#err3 <- mean(abs(pr3-sa3))
 
 # Aqui fem el mateix, pero establim que si la prediccio es <0.5, la interpreten com un 0, i 1 d'altre manera.
 # En les nostres proves l'error es de 0.1632653, exactament el mateix que err2.
 
-tobin <- function (x) { if (x >= 0.5) return (1); return (0) }
-pr4 <- sapply(pr3, tobin)
-err4 <- mean(abs(pr4-sa3))
+#tobin <- function (x) { if (x >= 0.5) return (1); return (0) }
+#pr4 <- sapply(pr3, tobin)
+#err4 <- mean(abs(pr4-sa3))
