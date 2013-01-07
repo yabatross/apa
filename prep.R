e02a0667dfb539641c409873c518329c59a48c24
yabatross <jeannekamikaze@gmail.com>
2013-01-07 17:42:19 +0100

    Added training and test sets

--- prep.R
+++ prep.R
@@ -1,5 +1,5 @@
 #
-# Preproceso
+# Preproces
 #
 encode_salary <- function (str)
 {
@@ -21,3 +21,23 @@ df$White         <- as.numeric(df$White)
 df$Male          <- as.numeric(df$Male)
 df$United.States <- as.numeric(df$United.States)
 df$X..50K        <- sapply(as.character(df$X..50K), encode_salary)
+
+# Training i test sets per cross validation
+
+s <- nrow (df)
+
+split1 <- s/3
+split2 <- 2*s/3
+
+chunk1 <- df[1:split1,]
+chunk2 <- df[split1:split2,]
+chunk3 <- df[split2:s,]
+
+train1 <- rbind (chunk1, chunk2)
+test1  <- chunk3
+
+train2 <- rbind (chunk1, chunk3)
+test2  <- chunk2
+
+train3 <- rbind (chunk2, chunk3)
+test3  <- chunk1
